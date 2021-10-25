4e704ef79faf7cd91c3fc974d07af594f0195e4f
AlbertDEV <81445004+AlbertRSCruz@users.noreply.github.com>
2021-10-24 21:05:08 -0300

    snapcrackle

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,26 @@
-// Seu código vai aqui
+let primeira = "snap";
+let segunda = "Crackle";
+let terceira = "SnapCrackle"
+let maxValue = 12;
+let valor = 1;
+let saidaArr = [];
+function snapCrackle(){
+    while(valor <= maxValue){
+         if (valor %5 === 0 && valor %2 !==0){
+        saidaArr.push(terceira);
+}
+         else if (valor %5 === 0){
+         saidaArr.push(segunda);
+}
+         else if (valor %5 === 0 || valor %2 !== 1) {
+         SaidaArr.push(valor);
+}
+        else if (valor %2 !== 0 ){
+            saidaArr.push(primeira);
+
+}
+valor++
+}
+return saidaArr
+}
+console.log (snapCrackle().join());
